a2c7bc7b2a3f87416ab2ea2d7d7c4b5cc1ec9907
Lucas BAUJARD <lubaujar@e2r4p9.42.fr>
2016-01-18 15:30:54 +0100

    Mon 3:30

--- srcs/arrows_and_movements/vertical_movements.c
+++ srcs/arrows_and_movements/vertical_movements.c
@@ -12,14 +12,39 @@
 
 #include "21sh.h"
 
+static int	count_char_to_shift(t_all *all)
+{
+	int		first_pos;
+	int		ret;
+
+	//printf("currLine : %d\n", all->curr_line);
+	first_pos = (all->curr_line == 1) ? 0 : 
+		(all->ws.ws_col * all->curr_line) + 1;
+	ret = 0;
+	//printf("pos curs : %d\n", all->cursor_pos);
+	while (first_pos++ <= all->cursor_pos)
+		ret++;
+//	printf("ret: %d\n", ret);
+	return (ret);
+}
+
 void	goto_up_line(t_all *all)
 {
+	int		ct;
+
+	ct = 0;
 	if (K_CTRL_UP && all->curr_line > 1)
 	{
 		//write(1, "here\n", 5);
 		tputs_termcap("up");
-		all->cursor_pos /= 2;
 		all->curr_line -= 1;
+		ct = count_char_to_shift(all);
+		// printf("pos curs : %d\n", all->cursor_pos);
+		//printf("currLine : %d\n", all->curr_line);
+		//printf("cursor: %d\n", all->cursor_pos);
+		all->cursor_pos = (all->curr_line == 1) ? 
+			(0 + ct) : ((all->ws.ws_col * all->curr_line) + ct);
+		//printf("cursor: %d\n", all->cursor_pos);
 	}
 }
 
@@ -30,11 +55,21 @@ void	goto_down_line(t_all *all)
 	ct = 0;
 	if (K_CTRL_DOWN && all->curr_line < all->nb_lines)
 	{
-		ct = (all->ws.ws_col * all->curr_line) + 1;
-		all->cursor_pos *= 2;
+		ct = count_char_to_shift(all);
+		// ct = (all->ws.ws_col * all->curr_line) + 1;
+		// all->cursor_pos *= 2;
+		// printf("pos curs : %d\n", all->cursor_pos);
+		// printf("currLine : %d\n", all->curr_line);
+
+	//	printf("pos2go : %d\n", (all->curr_line * all->ws.ws_col));
+		// printf("nb_move: %d\n", all->cursor_pos - ct);
 		tputs_termcap("do");
+	//	printf("cursor: %d\n", all->cursor_pos);
+		all->cursor_pos = (all->ws.ws_col * all->curr_line) + 1 + ct;
+	//	printf("cursor: %d\n", all->cursor_pos);
 		all->curr_line += 1;
-		while (ct++ < all->cursor_pos + 2)
+	//	printf("ct: %d\n", ct);
+		while (ct-- >= 0)
 		 	tputs_termcap("nd");
 		// if (all->curr_line == all->nb_lines
 		// 	&& all->cursor_pos > (int)all->cmd_termcaps->lenght)
@@ -48,19 +83,22 @@ void	goto_down_line(t_all *all)
 
 /*	=============================================================
 
-0123456789...
-$: .........|	0
-............|	1
-........	|	2
-			|	3
-			|	4
-			|	5
-			|	6
-			|	7
-			|	8
-			|	9
-
-
+   123456789 . . .
+0123456789 . . .
+$: ......9..|	0
+  <------> FirstPos + decalage
+  					+ 6 + 3 (prompt) 
+.........21..|	1
+.........33..|	2					cursPos : 7 -> 19 -> 31;
+.......		|	3					TermPos : 9 -> 21 -> 33;
+			|	4					1st ?   : 21;
+			|	5						==> : 9 * 2 = 18;
+			|	6						==> : 18 + 3 = 21;
+			|	7					----------------------
+			|	8						==> : termPos * 2 = res;
+			|	9						==> : res + lenPrompt = res2find;
+									----------------------
+										==> : cursPos 
 
 
 
