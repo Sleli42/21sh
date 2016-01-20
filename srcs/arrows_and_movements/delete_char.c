fce74ae1b776a757b537b6ffae0dbcc4fe2a2d2d
Lucas BAUJARD <lubaujar@e2r4p14.42.fr>
2016-01-20 19:41:52 +0100

    Wed 7:41

--- srcs/arrows_and_movements/delete_char.c
+++ srcs/arrows_and_movements/delete_char.c
@@ -12,6 +12,26 @@
 
 #include "21sh.h"
 
+void	del_char_multi_lines(t_all *all)
+{
+	int	ct;
+	int	save;
+
+	ct = all->curr_line;
+	save = all->cursor_pos;
+	create_cmd(all);
+	if ((int)ft_strlen(all->cmd) < ((all->nb_lines - 1) * all->ws.ws_col) - 3)
+		all->nb_lines--;
+	tputs_termcap("sc");
+	while (ct < all->nb_lines)
+	{
+		shift_first_char(all, ct);
+		ct++;
+	}
+	all->cursor_pos = save;
+	tputs_termcap("rc");
+}
+
 void	del_char(t_all *all)
 {
 	if (all->already_in_history)
@@ -24,35 +44,14 @@ void	del_char(t_all *all)
 			(all->current_key != K_BACKSPACE)
 			 ? dlst_del_one2(all->cmd_termcaps, all->cursor_pos)
 				: dlst_del_one2(all->cmd_termcaps, all->cursor_pos - 1);
-			if (all->nb_lines > 1)
-			{
-				int	ct = all->curr_line;
-				tputs_termcap("sc");
-				while (all->nb_lines - ct > 0)
-				{
-					// write(1, "turn\n", 5);
-					create_cmd(all);
-					if ((int)ft_strlen(all->cmd) < ((all->nb_lines - 1) * all->ws.ws_col) - 3)
-						all->nb_lines--;
-					shift_first_char(all, ct++);
-				}
-				tputs_termcap("rc");
-			}
 		}
 		(all->current_key == K_BACKSPACE)
 			? tputs_termcap("le") : NULL;
 		tputs_termcap("dc");
 		if (all->current_key == K_BACKSPACE)
 			all->cursor_pos--;
+		if (all->nb_lines > 1)
+			del_char_multi_lines(all);
 	}
 	tputs_termcap("ed");
 }
-
-
-/* ===================================================
-
-
-	a faire: shift multiligne
-	. . .
-
-===================================================== */
--- srcs/arrows_and_movements/horizontal_movements.c
+++ srcs/arrows_and_movements/horizontal_movements.c
@@ -95,35 +95,17 @@ static	void	reprint_char(t_all *all, t_cmd *nav)
 
 void	horizontal_moves(t_all *all)
 {
-	//all->curr_line = define_current_line(all, all->cursor_pos);
 	if (all->already_in_select
 		&& (size_t)all->cursor_pos < all->cmd_termcaps->lenght + 1)
 		reprint_char(all, goto_cursor_pos(all->cmd_termcaps->head, all->cursor_pos));
 	if (all->current_key == K_LEFT && all->cmd_termcaps->lenght > 0 && all->cursor_pos > 1)
 	{
-		// tputs_termcap("sr");
-		// if (all->curr_line == 2)
-		// {
-		// 	// if (all->cursor_pos == all->ws.ws_col)
-		// 	// 	all->curr_line++, all->nl = 0;
-		// 	printf("currLine: %d\n", all->curr_line);
-		// 	printf("res : %d\n", (all->curr_line - 1) * all->ws.ws_col);
-		// 	printf("allCurs: %d\n", all->cursor_pos + 1);
-		// }
-		//tputs_termcap("le");
-		 // printf("cursor2save: %d\n", all->cursor_pos);
-		 // printf("curr_line: %d\n", all->curr_line);
 		if (all->curr_line > 1 && all->cursor_pos + 2 == all->ws.ws_col * (all->curr_line - 1))
 		{
-			// printf("allCurs: %d\n", all->cursor_pos);
-			//all->cursor_pos--;
 			tputs_termcap("up");
 			all->curr_line--;
-		//	printf("lenLine: %d\n", all->ws.ws_col);
-		//	printf("currLine: %d\n", all->curr_line);
 			all->cursor_pos = (all->curr_line > 1) ? 
 				(all->ws.ws_col * (all->curr_line - 1)) + 1 : 1;
-		//	printf("allCurs: %d\n", all->cursor_pos);
 			while (all->cursor_pos < all->ws.ws_col * all->curr_line)
 			{
 				all->cursor_pos++;
@@ -140,21 +122,14 @@ void	horizontal_moves(t_all *all)
 	if (all->current_key == K_RIGHT && all->cmd_termcaps->lenght > 0
 		&&  (size_t)all->cursor_pos < all->cmd_termcaps->lenght + 1)
 	{
-		// tputs_termcap("sf");
-		// printf("currLine: %d\n", all->curr_line);
-		// printf("res : %d\n", all->curr_line * all->ws.ws_col);
-		//printf("allCurs: %d\n", all->cursor_pos);
 		if (all->cursor_pos + 3 == (all->ws.ws_col * all->curr_line))
 		{
-		//	write(1, "here\n", 5);
 			tputs_termcap("do");
 			all->curr_line++;
-			//tputs_termcap("le");
 		}
 		else
 			tputs_termcap("nd");
 		all->cursor_pos++;
-	//	printf("allCurs: %d\n", all->cursor_pos);
 		all->cpy_move_right += 
 			(all->already_in_select && all->cpy_move_left == 0) ? 1 : 0;
 	}
